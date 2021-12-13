5769c7a60b7a5723c3b935d6ebcd07d974fd4541
Miranda Ransom <mkr0129@hotmail.com>
2021-12-13 18:38:30 +0000

    psql_docker.sh script complete with create|start|stop

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -14,8 +14,6 @@ container_status=$(docker container inspect -f '{{.State.Status}}' jrvs-psql)
 #switch case to handle create|stop|start options
 case $cmd in
   create)
-    echo 'create selected'
-
     #check if container is already created
     if [ -n "$container_status" ]; then
       echo 'container already exists'
@@ -33,16 +31,10 @@ case $cmd in
     #create new volume if not exist
     docker volume create pgdata
     #create a container using psql image with name jrvs-psql
-    docker run --name jrvs-psql -e POSTGRES_PASSWORD="$db_password" -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres:9.6-alpine
-    #install psql CLI client
-    sudo yum install -y postgresql
-    #connect to psql instance using psql REPL
-    psql -h localhost -U postgres createuser "$db_username" -d postgres -W
-
+    docker run --name jrvs-psql -e POSTGRES_USER="$db_username" -e POSTGRES_PASSWORD="$db_password" -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres:9.6-alpine
+    exit $?
     ;;
   start|stop)
-    echo 'start or stop selected'
-
     if [ -z "$container_status" ]; then
       echo "Container does not exist"
       exit 1
