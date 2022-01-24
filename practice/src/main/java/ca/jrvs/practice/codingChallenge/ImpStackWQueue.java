d06036249cbf02e78a641686d575496fb9bd6f32
miranda0129 <miranda.ransom.0129@gmail.com>
2022-01-24 11:22:23 -0500

    implement stack with queue final

--- practice/src/main/java/ca/jrvs/practice/codingChallenge/ImpStackWQueue.java
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/ImpStackWQueue.java
@@ -1,2 +1,82 @@
-package ca.jrvs.practice.codingChallenge;public class ImpStackWQueue {
+package ca.jrvs.practice.codingChallenge;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * ticket: https://www.notion.so/jarvisdev/Implement-Stack-using-Queue-2b5637e90e55469687831297176d6e73
+ */
+public class ImpStackWQueue {
+  //implement stack using 2 queues
+  static Queue<Integer> pushQ = new LinkedList<>();
+  static Queue<Integer> popQ = new LinkedList<>();
+
+  int size = 0;
+
+  void push(int num){
+    size++;
+
+    pushQ.add(num);
+
+    while (!popQ.isEmpty()) {
+      pushQ.add(popQ.peek());
+      popQ.remove();
+    }
+
+    Queue<Integer> temp = popQ;
+    popQ = pushQ;
+    pushQ = temp;
+  }
+
+  int pop(){
+    if (popQ.isEmpty()) {
+      return -1;
+    }
+    size--;
+    return popQ.remove();
+  }
+
+  int peek(){
+    if (popQ.isEmpty()) {
+      return -1;
+    }
+    return popQ.peek();
+  }
+
+  int getSize(){
+    return size;
+  }
+
+  //implement stack using a single queue
+  public static class StackW1Queue{
+    Queue<Integer> queue = new LinkedList<>();
+
+    public void push(int num) {
+
+      queue.add(num);
+
+      for (int i = 0; i < queue.size()-1; i++) {
+        int x = queue.remove();
+        queue.add(x);
+      }
+    }
+
+    public int pop(){
+      if (queue.isEmpty()) {
+        return -1;
+      }
+      return queue.remove();
+    }
+
+    public int peek(){
+      if (queue.isEmpty()) {
+        return -1;
+      }
+      return queue.peek();
+    }
+
+    public int getSize(){
+      return queue.size();
+    }
+  }
 }
--- practice/src/test/java/ca/jrvs/practice/codingChallenge/ImpStackWQueueTest.java
+++ practice/src/test/java/ca/jrvs/practice/codingChallenge/ImpStackWQueueTest.java
@@ -1,2 +1,57 @@
-package ca.jrvs.practice.codingChallenge;public class ImpStackWQueueTest {
+package ca.jrvs.practice.codingChallenge;
+
+import ca.jrvs.practice.codingChallenge.ImpStackWQueue.StackW1Queue;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ImpStackWQueueTest {
+  ImpStackWQueue stack2 = new ImpStackWQueue();
+  StackW1Queue stack1 = new StackW1Queue();
+
+  //test implementation with 2 queues
+  @Test
+  public void testPush2(){
+    stack2.push(4);
+    Assert.assertEquals(1, stack2.getSize());
+    Assert.assertEquals(4, stack2.peek());
+  }
+
+  @Test
+  public void testPop2(){
+    stack2.push(1);
+    Assert.assertEquals(1, stack2.pop());
+    Assert.assertEquals(0, stack2.getSize());
+  }
+
+  @Test
+  public void testPeek2(){
+    stack2.push(1);
+    stack2.push(2);
+    stack2.push(3);
+    Assert.assertEquals(3, stack2.peek());
+  }
+
+  //test implementation with 1 queue
+  @Test
+  public void testPush1(){
+    stack1.push(1);
+    Assert.assertEquals(1, stack1.getSize());
+    Assert.assertEquals(1, stack1.pop());
+  }
+
+  @Test
+  public void testPop1() {
+    stack1.push(5);
+    Assert.assertEquals(5, stack1.pop());
+    Assert.assertEquals(0, stack1.getSize());
+  }
+
+  @Test
+  public void testPeek1() {
+    stack1.push(1);
+    stack1.push(2);
+    stack1.push(3);
+    Assert.assertEquals(3, stack1.peek());
+  }
+
 }
