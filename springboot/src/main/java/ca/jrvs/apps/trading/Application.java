3a3b867f8f69a138cee44c4797a8dee8f67af2a6
miranda0129 <miranda.ransom.0129@gmail.com>
2022-02-10 09:59:57 -0500

    DataMarket Dao: Implemention & Integreation Tests

--- springboot/src/main/java/ca/jrvs/apps/trading/Application.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/Application.java
@@ -1,7 +1,7 @@
 package ca.jrvs.apps.trading;
 
 class HelloWorld {
-public static void main(String args[]) {
-System.out.println("Hello World!");
-}
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
 }
