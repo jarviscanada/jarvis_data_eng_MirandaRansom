89614d2fec951ae03ac52b5eba7f071eba039d52
miranda0129 <miranda.ransom.0129@gmail.com>
2022-02-15 12:57:22 -0500

    Quote Service: Implementation and Testing

--- springboot/src/main/java/ca/jrvs/apps/trading/controller/QuoteController.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/controller/QuoteController.java
@@ -9,6 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -35,6 +36,16 @@ public class QuoteController {
     } catch (Exception e) {
       throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
           "Internal Error: please contact admin: " + e);
+    }
+  }
+
+  @PutMapping(path = "/iexMarketData")
+  @ResponseStatus(HttpStatus.OK)
+  public void updateMarketData() {
+    try {
+      quoteService.updateMarketData();
+    } catch (Exception e) {
+      throw ResponseExceptionUtil.getResponseStatusException(e);
     }
   }
 }
